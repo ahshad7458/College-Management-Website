13a79af8870a17bd230ef520776c61dcfcc3562c
Md Ahshad Alam <157880890+ahshad7458@users.noreply.github.com>
2026-08-24 17:16:15 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+    // root route http://localhost:5000/
+  res.send('Hello from the backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
